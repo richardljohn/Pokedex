f84b26e4eb9d3de3e8ba9095d9a1bb09f74efe6d
richardljohn <richardljohn@gmail.com>
2020-03-09 00:29:12 -0400

    Charmander Line

--- Pokedex.cpp
+++ Pokedex.cpp
@@ -3,13 +3,33 @@ using namespace std;
 
 enum Pokemon {Charmander, Charmeleon, Charizard};
 
-int main(){
-    string pokemon;
-    cout << "What Pokemon do you want?: ";
-    cin >> pokemon;
+Pokemon whoseThatPokemon(){
+    string name;
+    cout << "Enter the name of the Pokémon you want: "; 
+    cin >> name;
+    if(name == "Charmander"){
+        return Charmander;
+    }
+    if(name == "Charmeleon"){
+        return Charmeleon;
+    }
+    if(name == "Charizard"){
+        return Charizard;
+    }
+}
 
+int main(){
+    Pokemon pokemon = whoseThatPokemon();
     switch (pokemon){
         case Charmander:
-            cout << "Charmander\nType: Fire/Flying"; 
+            cout << "Charmander\nType: Fire" << endl; 
+            break;
+        case Charmeleon:
+            cout << "Charmeleon\nType: Fire\n";
+            break;
+        case Charizard:
+            cout << "Charizard\nType: Fire/Flying\n";
+            break;
     }
+    return 0;
 }
