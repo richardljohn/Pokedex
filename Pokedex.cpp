d5812bcaa8fe8dad3a4b4ea0fd40fa4ed152b434
richardljohn <richardljohn@gmail.com>
2020-03-11 16:00:14 -0400

    Whole Pokedex is handled by 2 methods and an enum.

--- Pokedex.cpp
+++ Pokedex.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std; 
 
-enum Pokemon {Bulbasaur = 1, Ivysaur, Venasaur, Charmander, Charmeleon, Charizard, Squirtle, Wartortle, Blastoise};
+enum Pokemon {Bulbasaur = 1, Ivysaur, Venasaur, Charmander, Charmeleon, Charizard, Squirtle, Wartortle, Blastoise, Caterpie, Metapod, Butterfree};
 
 Pokemon whoseThatPokemon(){
     int option, number; 
@@ -46,9 +46,8 @@ Pokemon whoseThatPokemon(){
     } 
 }
 
-int main(){
-    Pokemon pokemon = whoseThatPokemon();
-    switch (pokemon){
+void pokemonInfo(Pokemon poke){
+    switch (poke){
         case Bulbasaur:
             cout << "Bulbasaur\nType: Grass/Poison" << endl; 
             break;
@@ -77,5 +76,39 @@ int main(){
             cout << "Blastoise\nType: Water\n";
             break;
     }
+}
+
+int main(){
+    Pokemon pokemon = whoseThatPokemon();
+    pokemonInfo(pokemon);
+    // switch (pokemon){
+    //     case Bulbasaur:
+    //         cout << "Bulbasaur\nType: Grass/Poison" << endl; 
+    //         break;
+    //     case Ivysaur:
+    //         cout << "Ivysaur\nType: Grass/Poison\n";
+    //         break;
+    //     case Venasaur:
+    //         cout << "Venasaur\nType: Grass/Poison\n";
+    //         break;
+    //     case Charmander:
+    //         cout << "Charmander\nType: Fire" << endl; 
+    //         break;
+    //     case Charmeleon:
+    //         cout << "Charmeleon\nType: Fire\n";
+    //         break;
+    //     case Charizard:
+    //         cout << "Charizard\nType: Fire/Flying\n";
+    //         break;
+    //     case Squirtle:
+    //         cout << "Squirtle\nType: Water" << endl; 
+    //         break;
+    //     case Wartortle:
+    //         cout << "Wartortle\nType: Water\n";
+    //         break;
+    //     case Blastoise:
+    //         cout << "Blastoise\nType: Water\n";
+    //         break;
+    // }
     return 0;
 }
