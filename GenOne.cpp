5e9084b0d6117fcfb4f6aa0ae6365699de699bb1
richardljohn <richardljohn@gmail.com>
2020-03-12 01:27:52 -0400

    More Changes

--- GenOne.cpp
+++ GenOne.cpp
@@ -6,7 +6,7 @@ void GenOne::openFile(string file, Pokemon array[]){
     fstream gen1;
     int i = 0; 
     while(!gen1.eof()){
-        gen1 >> array[i].num >> array[i].name >> array[i].type >> array[i].type2;
+        gen1 >> array[i].name >> array[i].type >> array[i].type2;
         i++;
     }
     gen1.close();
@@ -15,6 +15,7 @@ void GenOne::openFile(string file, Pokemon array[]){
 void GenOne::display(Pokemon array[]){
     int i = 0; 
     while(i < 50){
+        array[i].num = i+1; 
         if(array[i].type2 == "None"){
             array[i].type2 = "";
         }
